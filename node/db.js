423f2cc8d80abd1f7a55900b978023b38f1e20d1
Matt Ross <matt@smartsrc.com>
2014-12-30 20:19:24 -0800

    change server

--- node/db.js
+++ node/db.js
@@ -2,7 +2,8 @@ var mongoose = require('mongoose'),
     Post = require('./models/posts-model').Post;
 
 //mongoose.connect('mongodb://usefuljs:usefulpass@kahana.mongohq.com:10062/go_test')
-mongoose.connect('mongodb://nodejs-ssmdd-01.smartsource.local:27017/usefulJS?auto_reconnect=true')
+mongoose.connect('mongodb://mbahoshy:07maryJ68@ds029801.mongolab.com:29801/usefuljs')
+//mongoose.connect('mongodb://nodejs-ssmdd-01.smartsource.local:27017/usefulJS?auto_reconnect=true')
 
 var db = mongoose.connection;
 
